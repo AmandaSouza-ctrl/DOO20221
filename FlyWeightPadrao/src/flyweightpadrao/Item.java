4161b4cc0be846c221f329ce62369e38aa7da7dd
Amanda Souza <55111387+AmandaSouza-ctrl@users.noreply.github.com>
2022-05-12 09:08:18 -0300

    Add files via upload

---
+++ FlyWeightPadrao/src/flyweightpadrao/Item.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flyweightpadrao;
+
+/**
+ *
+ * @author 20181BSI0148
+ */
+class Item {
+
+    void setDescricao(String lapis_de_cor_modelo_xyz) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setNome(String lapis) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
