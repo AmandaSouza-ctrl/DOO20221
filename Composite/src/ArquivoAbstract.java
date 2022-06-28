4f8d1bda030bef6c611826b19aa9e6e454bd503e
Amanda Souza <55111387+AmandaSouza-ctrl@users.noreply.github.com>
2022-06-28 08:49:20 -0300

    Add files via upload

---
+++ Composite/src/ArquivoAbstract.java
@@ -0,0 +1,8 @@
+
+abstract class ArquivoAbstract {
+    public String nome;
+    
+    public void printNomeDoArquivo(){
+        System.out.println(nome);
+    }
+}
---
+++ Composite/src/ArquivoComposite.java
@@ -0,0 +1,13 @@
+
+import java.util.ArrayList;
+
+
+public class ArquivoComposite extends ArquivoAbstract {
+    public ArrayList listaDeArquivos(){
+        ArrayList list = new ArrayList();
+        
+        return list;
+    }
+    
+    
+}
